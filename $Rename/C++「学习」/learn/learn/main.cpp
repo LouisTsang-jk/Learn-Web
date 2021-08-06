ecd1c91be3f3e4be42ddd31a9664facc16be61c1
曾俊坤 <zengjunkun@cvte.com>
2021-08-06 09:39:33 +0800

    add: log

--- $Rename/C++「学习」/learn/learn/main.cpp
+++ $Rename/C++「学习」/learn/learn/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-#include "Sales_item.h"
+//#include "Sales_item.h"
 int main () {
-    Sales_item item1, item2;
-    std::cin >> item1 >> item2;
-    std::cout << item1 + item2 << std::endl;
+//    Sales_item item1, item2;
+//    std::cin >> item1 >> item2;
+//    std::cout << item1 + item2 << std::endl;
+    int ival = 1024;
+    int ival2 = 1024;
+    int &refVal = ival;
+    int fakeVal = ival;
+    int &refVal2 = ival2;
+    std::cout << refVal << std::endl;
+    std::cout << fakeVal << std::endl;
+    ival = ival + 100;
+    std::cout << ival << std::endl;
+    std::cout << fakeVal << std::endl;
+    std::cout << refVal << std::endl;
+    std::cout << refVal2 << std::endl;
     return 0;
 }
