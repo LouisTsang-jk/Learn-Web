99b092ab6863a53b58661546b49482e625e2072f
曾俊坤 <zengjunkun@cvte.com>
2021-08-23 21:20:17 +0800

    add:sth

--- $Rename/C++「学习」/learn/learn/main.cpp
+++ $Rename/C++「学习」/learn/learn/main.cpp
@@ -4,11 +4,5 @@ int main () {
 //    Sales_item item1, item2;
 //    std::cin >> item1 >> item2;
 //    std::cout << item1 + item2 << std::endl;
-    int ival = 42;
-    int *p = &ival;
-    std::cout << ival << "-" << *p << std::endl;
-    ival += 5;
-    std::cout << ival << "-" << *p << std::endl;
-    std::cout << p << "-" << &ival << std::endl;
     return 0;
 }
