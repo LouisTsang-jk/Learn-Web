1630b6557dbb98976f61d9b6df9ef4762d6070a4
LouisTsang <chiyamakagami@foxmail.com>
2022-03-10 23:12:10 +0800

    add log

---
+++ $Rename/Hvísla「个人简介」/「知识库」Lib/「手写代码」Handwrite/call.js
@@ -0,0 +1,10 @@
+Function.prototype.call = function (context) {
+  context = context ? Object(context) : window;
+  context.fn = this;
+
+  const args = [...arguments].slice(1);
+  const result = context.fn(...args);
+
+  delete context.fn;
+  return result;
+};
