884e2ab867ddf57d476658500bcbc3415436d81a
LouisTsang <chiyamakagami@foxmail.com>
2022-03-11 18:06:38 +0800

    add log

--- $Rename/Hvísla「个人简介」/「知识库」Lib/「手写代码」Handwrite/debounce.js
+++ $Rename/Hvísla「个人简介」/「知识库」Lib/「手写代码」Handwrite/debounce.js
@@ -1,12 +1,17 @@
-function debounce (fn, wait) {
-  // 通过闭包缓存定时器
-  let timer = null;
+function debounce(fn, time) {
+  let timer;
   return function (...args) {
-    // 将this保存到context中
-    const context = this;
     timer && clearTimeout(timer);
+    const context = this;
     timer = setTimeout(() => {
       fn.apply(context, args);
-    }, wait);
-  }
+    }, time);
+  };
 }
+
+const div = document.createElement("div");
+div.style = "height: 300vh;width: 100px;border: 1px solid #000";
+document.body.appendChild(div);
+document.body.onscroll = debounce((e) => {
+  console.log("scroll", e);
+}, 1000);
--- $Rename/Hvísla「个人简介」/「知识库」Lib/「手写代码」Handwrite/throttle.js
+++ $Rename/Hvísla「个人简介」/「知识库」Lib/「手写代码」Handwrite/throttle.js
@@ -1,11 +1,21 @@
-function throttle(fn, delay = 200) {
+function throttle(fn, time) {
   let flag = true;
   return function (...args) {
-    if (!flag) return;
-    flag = false;
-    setTimeout(() => {
-      fn.apply(this, args);
-      flag = true;
-    }, delay);
-  };
+      if (!flag) return;
+      flag = false;
+      const context = this;
+      setTimeout(() => {
+          fn.apply(context, args);
+          flag = true;
+      }, time)
+  }
 }
+
+
+const div = document.createElement('div');
+div.style = 'height: 300vh;width: 100px;border: 1px solid #000';
+document.body.appendChild(div);
+document.body.onscroll = throttle((e) => {
+  console.log('scroll', e);
+}, 1000)
+
